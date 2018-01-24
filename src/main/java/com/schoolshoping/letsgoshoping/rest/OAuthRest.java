9d2c7b3656d121d3d5354f40dbe7db3c19bf8dca
zb-thomas <chengfang@zebao-china.com>
2018-01-24 10:45:25 +0800

    result提交

--- src/main/java/com/schoolshoping/letsgoshoping/rest/OAuthRest.java
+++ src/main/java/com/schoolshoping/letsgoshoping/rest/OAuthRest.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.schoolshoping.letsgoshoping.base.Result;
 
 
+
 @RestController
 @RequestMapping("/schoolshoping")
 public class OAuthRest {
--- src/main/java/com/schoolshoping/letsgoshoping/service/SchoolService.java
+++ src/main/java/com/schoolshoping/letsgoshoping/service/SchoolService.java
@@ -1,5 +1,8 @@
 package com.schoolshoping.letsgoshoping.service;
+
 import com.schoolshoping.letsgoshoping.base.Result;
+
+
 public interface SchoolService {
 	Result findbyName(String name);
 	Result findAll();
